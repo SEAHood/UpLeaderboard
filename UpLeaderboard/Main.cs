d896a607047dc82c9965d5869cbcc7833f56af3c
Sam Hood <me@samhood.net>
2019-04-17 23:39:01 +0100

    Fixed leaderboard

--- UpLeaderboard/Main.cs
+++ UpLeaderboard/Main.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +7,9 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace UpLeaderboard
 {
@@ -26,6 +25,8 @@ namespace UpLeaderboard
         public string Name { get; set; }
         [JsonProperty("score")]
         public int Score { get; set; }
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
     }
 
     public static class Main
@@ -33,17 +34,22 @@ namespace UpLeaderboard
         [FunctionName("Leaderboard")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [Blob("leaderboards/leaderboard.json", FileAccess.Read)] Stream inBlob,
-            [Blob("leaderboards/leaderboard.json", FileAccess.Write)] Stream outBlob,
+            [StorageAccount("AzureWebJobsStorage")] CloudStorageAccount storageAccount,
             ILogger log)
         {
             try
             {
+                var client = storageAccount.CreateCloudBlobClient();
+                var container = client.GetContainerReference("leaderboards");
+                await container.CreateIfNotExistsAsync();
+
+                var leaderboardBlob = container.GetBlockBlobReference("leaderboard.json");
                 var leaderboardJson = "";
+
                 switch (req.Method.ToLower())
                 {
                     case "get":
-                        leaderboardJson = inBlob != null ? GetLeaderboard(inBlob) : JsonConvert.SerializeObject(new Leaderboard());
+                        leaderboardJson = await leaderboardBlob.DownloadTextAsync();
                         break;
                     case "post":
                         dynamic data = JsonConvert.DeserializeObject(await req.ReadAsStringAsync());
@@ -51,7 +57,7 @@ namespace UpLeaderboard
                         var score = (string)data.score;
                         if (name == null || score == null || name == "" || !int.TryParse(score, out var scoreInt)) 
                             return new BadRequestResult();
-                        leaderboardJson = PostScore(inBlob, outBlob, name, scoreInt);
+                        leaderboardJson = await PostScore(leaderboardBlob, name, scoreInt, log);
                         break;
                 }
 
@@ -64,40 +70,27 @@ namespace UpLeaderboard
             }
         }
 
-        private static string GetLeaderboard(Stream inBlob)
-        {
-            using (var sw = new StreamReader(inBlob))
-            {
-                var body = sw.ReadToEnd();
-                return body;
-            }
-        }
-
-        private static string PostScore(Stream inBlob, Stream outBlob, string name, int score)
+        private static async Task<string> PostScore(CloudBlockBlob leaderboard, string name, int score, ILogger log)
         {
             Leaderboard board;
-            if (inBlob == null)
+            try
+            {
+                board = JsonConvert.DeserializeObject<Leaderboard>(await leaderboard.DownloadTextAsync());
+
+                if (board.Scores == null)
+                    board.Scores = new List<PlayerScore>();
+            }
+            catch (Exception e) // File is probably corrupted somehow
+            {
                 board = new Leaderboard();
-            else
-            {
-                using (var sw = new StreamReader(inBlob))
-                {
-                    var body = sw.ReadToEnd();
-                    board = JsonConvert.DeserializeObject<Leaderboard>(body);
-                }
-
-                if (board == null)
-                    board = new Leaderboard();
-            }
-            
-            board.Scores.Add(new PlayerScore {Name = name, Score = score});
-
-            using (var sw = new StreamWriter(outBlob, Encoding.Unicode))
-            {
-                sw.Write(JsonConvert.SerializeObject(board));
-                sw.Flush();
+                log.LogError(e.Message);
             }
 
+            var newScore = new PlayerScore { Name = name, Score = score, Timestamp = DateTime.UtcNow };
+            board.Scores.Add(newScore);
+            await leaderboard.UploadTextAsync(JsonConvert.SerializeObject(board));
+
+            log.LogInformation($"Score logged: {JsonConvert.SerializeObject(newScore)}");
             return JsonConvert.SerializeObject(board);
         }
     }
